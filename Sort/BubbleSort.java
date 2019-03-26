9204ee0ea0454579521bf1eea84e7d41ec7c67b6
ZequnSong <36482357+ZequnSong@users.noreply.github.com>
2019-03-25 23:18:09 -0400

    Add files via upload

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,60 @@
+package Sort.BubbleSort;
+
+/**
+ * BubbleSort
+ *  Author : Zequn Song
+ *  Email : zsong73@gwu.edu
+ */
+public class BubbleSort {
+
+    /**
+     * BubbleSort
+     * @param array
+     * @return
+     */
+    public static int [] sort(int[] array){
+        //for loop here means how many loops in all do we need to compare
+        for(int i = 1; i < array.length; i++){
+            //flag true means no exchange in current loop
+            boolean flag = true;
+
+            //for loop here means the index of elements in each round of comparison
+            //careful about the range of j is dynamic
+            for(int j = 0; j < array.length - i; j++){
+                if(array[j] > array[j + 1]){
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                    flag = false;
+                }
+            }
+            if(flag)
+                break;
+            System.out.print("result of "+ i + "th comparison: ");
+            display(array);
+        }
+        return array;
+    }
+
+    /**
+     * iterate to show the array
+     * @param array
+     */
+    public static void display(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] ars) {
+        int[] array = {5, 9, 4, 7, 8, 1, 6, 3};
+        System.out.println("Un-sorted array: ");
+        display(array);
+        System.out.println("-----Bubble Sort Process-----");
+        array = sort(array);
+        System.out.println("-----------------------------");
+        System.out.println("Sorted array: ");
+        display(array);
+    }
+}
