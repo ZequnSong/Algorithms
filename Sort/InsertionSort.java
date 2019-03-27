76247dddc65dc5fc20bf90df9957938124d0c1a1
ZequnSong <36482357+ZequnSong@users.noreply.github.com>
2019-03-27 13:50:10 -0400

    Update InsertionSort.java

--- Sort/InsertionSort.java
+++ Sort/InsertionSort.java
@@ -13,20 +13,20 @@ public class InsertionSort {
      * @return
      */
     public static int[] sort(int[] array){
-        //start sorting from the element with index 1, cause the first element (index 0) is default sorted
+       //start sorting from the element with index 1, cause the first element (index 0) is default sorted
         for(int i = 1; i < array.length; i++){
             //j is the position for array[i] to insert in, start from the tail of the sorted elements
-            int j = i;
+            int j = i - 1;
             int tmp = array[i];
             //from the tail of the sorted, all elements that bigger than array[i] move one step back
             //stop until find one smaller than array[i]
-            while(j > 0 && array[j-1] > tmp){
-                array[j] = array[j-1]; //data move backward
+            while(j >= 0 && array[j] > tmp){
+                array[j+1] = array[j]; //data move backward
                 j--;
             }
             //breaking out the loop means array[j-1] < tmp
             //therefore array[j] is the position for array[i]
-            array[j] = tmp;
+            array[j+1] = tmp;
             System.out.print("result of "+ i  + "th comparison: ");
             display(array);
         }
